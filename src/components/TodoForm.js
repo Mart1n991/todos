c448371869edd2316acbe5522281fca2fccc2d48
Mart1n991 <martinjavorsky8@gmail.com>
2020-12-03 17:49:27 +0100

    Pridal som element select s možnosťami filtrovania todos

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -6,6 +6,12 @@ export default function TodoForm({ items, handleChange, handleSubmit, value }) {
       <label htmlFor="new-todo">What needs to be done?</label>
       <input autoFocus id="new-todo" onChange={handleChange} value={value} />
       <button>{`Add #${items.length + 1}`}</button>
+
+      <select>
+        <option>All</option>
+        <option>Completed</option>
+        <option>Uncompleted</option>
+      </select>
     </form>
   );
 }
