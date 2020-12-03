f5cc5fd18aafee0468e622b7fc6fea32298edc0a
Mart1n991 <martinjavorsky8@gmail.com>
2020-12-03 17:44:50 +0100

    Nainštaloval som knižnicu fontAwesome

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -4,7 +4,7 @@ export default function TodoForm({ items, handleChange, handleSubmit, value }) {
   return (
     <form onSubmit={handleSubmit}>
       <label htmlFor="new-todo">What needs to be done?</label>
-      <input id="new-todo" onChange={handleChange} value={value} />
+      <input autoFocus id="new-todo" onChange={handleChange} value={value} />
       <button>{`Add #${items.length + 1}`}</button>
     </form>
   );
